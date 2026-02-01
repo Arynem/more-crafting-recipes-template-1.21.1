3196bca80a5333a09c8af6a8caaf3fdb8c7909da
Arynem <ryan.emry@outlook.com>
2026-01-31 21:16:19 -0600

    1.0.0

---
+++ src/main/java/com/arynem/morecraftingrecipes/MoreCraftingClient.java
@@ -0,0 +1,10 @@
+package com.arynem.morecraftingrecipes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreCraftingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/arynem/morecraftingrecipes/MoreCraftingRecipes.java
@@ -0,0 +1,15 @@
+package com.arynem.morecraftingrecipes;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreCraftingRecipes implements ModInitializer {
+	public static final String MOD_ID = "more-crafting-recipes";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
